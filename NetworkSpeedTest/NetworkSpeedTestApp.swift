aa093868ca015d175ccf11272437e370c0efef38
Mikhail Demichev <demichev.mi@gmail.com>
2024-04-24 13:37:32 +0700

    Initial Commit

---
+++ NetworkSpeedTest/NetworkSpeedTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkSpeedTestApp.swift
+//  NetworkSpeedTest
+//
+//  Created by Mikhail Demichev on 24.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkSpeedTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
